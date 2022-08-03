f69228ba507f1c43052dcbaeddfd187d46974b5c
Jocelle Bautista <jcllebautista@gmail.com>
2022-08-03 16:46:41 -0400

    add locations

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Zach", "Erik", "Chris", "Kim", "Elisa"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+var locations = ["Nashville", "Japan", "Italy", "Norway", "Dubai"];
+locations.push("Austin");
+console.log(locations);
